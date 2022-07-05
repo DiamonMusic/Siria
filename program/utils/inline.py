73582e0f6cfa5f8056d9c5f9fd52d43568a4c669
DiamonMusic <108665603+DiamonMusic@users.noreply.github.com>
2022-07-05 11:50:33 +0200

    Add files via upload

---
+++ program/utils/inline.py
@@ -0,0 +1,63 @@
+""" inline section button """
+
+from pyrogram.types import (
+  CallbackQuery,
+  InlineKeyboardButton,
+  InlineKeyboardMarkup,
+  Message,
+)
+
+
+def stream_markup(user_id):
+  buttons = [
+    [
+      InlineKeyboardButton(text="⌔ القائمه", callback_data=f'cbmenu | {user_id}'),
+      InlineKeyboardButton(text="Sَoٰuِِ𝖱ٰcٌe ٰDٍِiٰaِmٓoِٰ𝗇ٰٰDِِ", url=f'https://t.me/CR_T2'),
+    ],
+    [
+    InlineKeyboardButton(
+                        "ضيف البوت في جروبك ➕",
+                        url=f'https://t.me/{BOT_USERNAME}?startgroup=true'),
+    ],
+  ]
+  return buttons
+
+
+def menu_markup(user_id):
+  buttons = [
+    [
+      InlineKeyboardButton(text="⏹", callback_data=f'cbstop | {user_id}'),
+      InlineKeyboardButton(text="⏸", callback_data=f'cbpause | {user_id}'),
+      InlineKeyboardButton(text="▶️", callback_data=f'cbresume | {user_id}'),
+    ],
+    [
+      InlineKeyboardButton(text="🔇", callback_data=f'cbmute | {user_id}'),
+      InlineKeyboardButton(text="🔊", callback_data=f'cbunmute | {user_id}'),
+    ],
+    [
+      InlineKeyboardButton(text="🗑 اغلاق", callback_data='cls'),
+    ]
+  ]
+  return buttons
+
+
+close_mark = InlineKeyboardMarkup(
+  [
+    [
+      InlineKeyboardButton(
+        "🔙 رجوع", callback_data="cbmenu"
+      )
+    ]
+  ]
+)
+
+
+back_mark = InlineKeyboardMarkup(
+  [
+    [
+      InlineKeyboardButton(
+        "🔙 رجوع", callback_data="cbmenu"
+      )
+    ]
+  ]
+)
